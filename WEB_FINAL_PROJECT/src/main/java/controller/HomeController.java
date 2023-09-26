ab75913c328239567c5591a2ad57b016ab4e8541
Tuavsn <hoctuavsn@gmail.com>
2023-09-26 14:39:37 +0700

    update

--- WEB_FINAL_PROJECT/src/main/java/controller/HomeController.java
+++ WEB_FINAL_PROJECT/src/main/java/controller/HomeController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,13 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.vienmv.service.ProductService;
+import com.vienmv.service.impl.ProductServiceImpl;
+
+import model.CategoryModel;
+import service.CategoryService;
+import serviceImpl.CategoryServiceImpl;
+
 @WebServlet(urlPatterns = {"/home"})
 public class HomeController extends HttpServlet{
+	CategoryService categoryservice = new CategoryServiceImpl();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		resp.setCharacterEncoding("UTF-8");
 		req.setCharacterEncoding("UTF-8");
+		
+		List<CategoryModel> allCategory = categoryservice.findAll();
 		
 		RequestDispatcher rq = req.getRequestDispatcher("views/home.jsp");
 		rq.forward(req, resp);
