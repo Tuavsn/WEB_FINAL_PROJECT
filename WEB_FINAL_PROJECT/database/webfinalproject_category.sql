1224299825ba65a73152c79cfbe76333ba1c0d09
Tuavsn <hoctuavsn@gmail.com>
2023-09-27 09:57:46 +0700

    refactor database

--- WEB_FINAL_PROJECT/database/webfinalproject_category.sql
+++ WEB_FINAL_PROJECT/database/webfinalproject_category.sql
@@ -25,7 +25,8 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `CategoryID` int NOT NULL,
   `CategoryName` varchar(50) DEFAULT NULL,
-  `Icon` varchar(45) DEFAULT NULL,
+  `ImageLink` varchar(300) DEFAULT NULL,
+  `Icon` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`CategoryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +37,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Chăm sóc da mặt',NULL),(2,'Chăm sóc cơ thể',NULL),(3,'Trang điểm',NULL),(4,'Thực phẩm chức năng',NULL),(5,'Dược mỹ phẩm',NULL),(6,'Phụ kiện',NULL);
+INSERT INTO `category` VALUES (1,'Chăm sóc da mặt','https://evprincessbichlien.com/wp-content/uploads/2019/11/tai-sao-phai-cham-soc-da-mat.jpg','<i class=\"fa fa-user\"></i>'),(2,'Chăm sóc cơ thể','https://cdn.tgdd.vn/Files/2020/09/09/1288510/sua-tam-puri-co-tot-khong-dac-diem-huong-thom-va-gia-thanh-cua-tung-loai-202212020807178476.jpg','<i class=\"fa fa-child\"></i>'),(3,'Trang điểm','https://worldnail.edu.vn/Upload/images/khoa-hoc-trang-diem-ca-nhan.jpg','<i class=\"fab fa-gitlab\"></i>'),(4,'Thực phẩm chức năng','https://medlatec.vn/media/11756/content/20210309_thuc-pham-chuc-nang-la-gi1.jpg','<i class=\"fa fa-pills\"></i>'),(5,'Dược mỹ phẩm','https://images2.thanhnien.vn/Uploaded/dieutrang-qc/2021_10_22/mai-han-duoc-my-pham-2-4439.png','<i class=\"fa fa-capsules\"></i>'),(6,'Phụ kiện','https://cdn.vuahanghieu.com/unsafe/1200x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/news/2021/01/hoa-my-pham-la-gi-phan-tich-uu-nhuoc-diem-cua-hoa-my-pham-12012021112958.jpg','<i class=\"fa fa-list\"></i>');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-26 18:35:38
+-- Dump completed on 2023-09-27  9:50:55
