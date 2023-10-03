da98a4fc7879b8f125ea2c16421dacd0acdfa81e
Tuavsn <hoctuavsn@gmail.com>
2023-10-03 14:40:23 +0700

    update database

--- WEB_FINAL_PROJECT/database/inseriroleanduser.sql
+++
@@ -1,7 +0,0 @@
-
-insert into role(code,name) values('ADMIN','ADMIN');
-insert into role(code,name) values('USER','USER');
-
-INSERT INTO user (username, password, fullname, status, roleid,sdt) VALUES ('admin', '123456', 'admin', 1, 1,0367386108);
-insert into user(username,password,fullname,status, roleid,sdt) values('nguyenvana','123456','Nguyễn Văn a',1,2,1234123);
-insert into user(username,password,fullname,status, roleid,sdt) values('nguyenvanb','123456','Nguyễn Văn ba',1,2,1234123);
