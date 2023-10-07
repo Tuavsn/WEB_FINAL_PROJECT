c20ea197ba51b5091b9237aed86f14e4546447d7
dtoan <dtoan@Toan>
2023-10-07 07:06:41 +0700

    data

--- WEB_FINAL_PROJECT/database/webfinalproject_category.sql
+++ WEB_FINAL_PROJECT/database/webfinalproject_category.sql
@@ -27,8 +27,9 @@ CREATE TABLE `category` (
   `CategoryName` varchar(50) DEFAULT NULL,
   `ImageLink` varchar(300) DEFAULT NULL,
   `Icon` varchar(100) DEFAULT NULL,
+  `ParentID` bigint NOT NULL,
   PRIMARY KEY (`CategoryID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +38,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Chăm sóc da mặt','https://evprincessbichlien.com/wp-content/uploads/2019/11/tai-sao-phai-cham-soc-da-mat.jpg','<i class=\"fa fa-user\"></i>'),(2,'Chăm sóc cơ thể','https://cdn.tgdd.vn/Files/2020/09/09/1288510/sua-tam-puri-co-tot-khong-dac-diem-huong-thom-va-gia-thanh-cua-tung-loai-202212020807178476.jpg','<i class=\"fa fa-child\"></i>'),(3,'Trang điểm','https://worldnail.edu.vn/Upload/images/khoa-hoc-trang-diem-ca-nhan.jpg','<i class=\"fab fa-gitlab\"></i>'),(4,'Thực phẩm chức năng','https://medlatec.vn/media/11756/content/20210309_thuc-pham-chuc-nang-la-gi1.jpg','<i class=\"fa fa-pills\"></i>'),(5,'Dược mỹ phẩm','https://images2.thanhnien.vn/Uploaded/dieutrang-qc/2021_10_22/mai-han-duoc-my-pham-2-4439.png','<i class=\"fa fa-capsules\"></i>'),(6,'Phụ kiện','https://cdn.vuahanghieu.com/unsafe/1200x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/news/2021/01/hoa-my-pham-la-gi-phan-tich-uu-nhuoc-diem-cua-hoa-my-pham-12012021112958.jpg','<i class=\"fa fa-list\"></i>');
+INSERT INTO `category` VALUES (1,'Chăm sóc da mặt','https://evprincessbichlien.com/wp-content/uploads/2019/11/tai-sao-phai-cham-soc-da-mat.jpg','<i class=\"fa fa-user\"></i>',0),(2,'Chăm sóc cơ thể','https://cdn.tgdd.vn/Files/2020/09/09/1288510/sua-tam-puri-co-tot-khong-dac-diem-huong-thom-va-gia-thanh-cua-tung-loai-202212020807178476.jpg','<i class=\"fa fa-child\"></i>',0),(3,'Trang điểm','https://worldnail.edu.vn/Upload/images/khoa-hoc-trang-diem-ca-nhan.jpg','<i class=\"fab fa-gitlab\"></i>',0),(4,'Thực phẩm chức năng','https://medlatec.vn/media/11756/content/20210309_thuc-pham-chuc-nang-la-gi1.jpg','<i class=\"fa fa-pills\"></i>',0),(5,'Dược mỹ phẩm','https://images2.thanhnien.vn/Uploaded/dieutrang-qc/2021_10_22/mai-han-duoc-my-pham-2-4439.png','<i class=\"fa fa-capsules\"></i>',0),(6,'Phụ kiện','https://cdn.vuahanghieu.com/unsafe/1200x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/news/2021/01/hoa-my-pham-la-gi-phan-tich-uu-nhuoc-diem-cua-hoa-my-pham-12012021112958.jpg','<i class=\"fa fa-list\"></i>',0),(7,'Tẩy trang','',NULL,1),(8,'Sữa rửa mặt','',NULL,1),(9,'Serum','',NULL,1),(10,'Xịt khoáng','',NULL,1),(11,'Mặt nạ','',NULL,1),(12,'Dưỡng mắt','',NULL,1),(13,'Dưỡng môi','',NULL,1),(14,'Sữa tắm','',NULL,2),(15,'Xà phòng','',NULL,2),(16,'Tẩy tế bào chết body','',NULL,2),(17,'Dưỡng thể','',NULL,2),(18,'Khử mùi','',NULL,2),(19,'Kem chống nắng','',NULL,1),(20,'Kem dưỡng da','',NULL,1),(21,'Kem lót','',NULL,3),(22,'Kem nền','',NULL,3),(23,'Son','',NULL,3),(24,'Kẻ mắt','',NULL,3),(25,'Hỗ trợ da','',NULL,4),(26,'Hỗ trợ tóc',NULL,NULL,4),(27,'Hỗ trợ giảm cân',NULL,NULL,4),(28,'Vitamin/ Khoáng chất',NULL,NULL,4);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-05 21:05:19
+-- Dump completed on 2023-10-07  0:00:10
